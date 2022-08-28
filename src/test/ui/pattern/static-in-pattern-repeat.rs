f84237d5cf5b4122cbefaf2124546164f0023024
Jules Bertholet <julesbertholet@quoi.xyz>
2022-08-28 00:36:27 -0400

    Revert "Re-add reachability check for references one level deep"
    This reverts commit 4f1539f965194ed273d9bc71c5bbd791d9a044df.

--- src/test/ui/pattern/static-in-pattern-repeat.rs
+++ src/test/ui/pattern/static-in-pattern-repeat.rs
@@ -2,8 +2,6 @@
 #![deny(unreachable_patterns)]
 
 static ZERO: i32 = 0;
-static ZERO_REF: &i32 = &0;
-
 
 fn main() {
     let val = match 0 {
@@ -12,12 +10,4 @@ fn main() {
         //~^ ERROR unreachable pattern
         _ => false,
     };
-
-    let val = match &0 {
-        ZERO_REF => true,
-        ZERO_REF => false,
-        //~^ ERROR unreachable pattern
-        _ => false,
-    };
-    assert_eq!(val, true);
 }
